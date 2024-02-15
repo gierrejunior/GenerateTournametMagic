cd4b27ab0fa1e46674cf03caabfb625cecdd28b6
Gierre <gierremartins@gmail.com>
2024-02-15 15:19:03 -0300

    V.1.0.0

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 from interfaces.list_manager import ListManager
 from magic_modules.generator_magic_duels import DuelsType
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Adiciona jogadores
-    item_digitado_player = st.text_input(label='Digite um player:')
+    item_digitado_player = st.text_input(label="Digite um player:")
     player_manager = ListManager("players", "player")
     if st.button(f"Adicionar {player_manager.item_label}"):
         player_manager.add_item(item_digitado_player)
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     player_manager.display()
 
     # Adiciona decks
-    item_digitado_deck = st.text_input(label='Digite um deck:')
+    item_digitado_deck = st.text_input(label="Digite um deck:")
     deck_manager = ListManager("decks", "deck")
     if st.button(f"Adicionar {deck_manager.item_label}"):
         deck_manager.add_item(item_digitado_deck)
@@ -23,14 +23,12 @@ if __name__ == '__main__':
     deck_manager.display()
 
     # Adiciona uma janela para selecionar múltiplos players do último duelo
-    last_duel = st.multiselect(
-        label='Último duelo',
-        options=st.session_state.players
-    )
+    last_duel = st.multiselect(label="Último duelo", options=st.session_state.players)
 
     if len(last_duel) > 2:
         st.error(
-            'Por favor, selecione no máximo dois players que duelaram no último jogo')
+            "Por favor, selecione no máximo dois players que duelaram no último jogo"
+        )
     elif len(last_duel) == 2:
         st.write(f"Último duelo: {', '.join(last_duel)}")
         # Aqui você pode adicionar os jogadores à sua variável ou fazer qualquer outra operação necessária
@@ -40,60 +38,67 @@ if __name__ == '__main__':
         # Verifica se o modo torneio é válido
         if len(st.session_state.players) % 2 == 0:
             modo = st.radio(
-                label='Escolha o modo:',
-                options=['Duelo simples', 'Torneio'],
+                label="Escolha o modo:",
+                options=["Duelo simples", "Torneio"],
                 horizontal=True,
-                index=0
+                index=0,
             )
             st.write(f"Modo escolhido: {modo}")
         else:
             modo = st.radio(
-                label='Escolha o modo:',
-                options=['Duelo simples'],
+                label="Escolha o modo:",
+                options=["Duelo simples"],
                 horizontal=True,
                 index=0,
             )
-            st.error(
-                'O modo torneio só fica ativo se o número de jogadores for par.')
+            st.error("O modo torneio só fica ativo se o número de jogadores for par.")
             st.write(f"Modo escolhido: {modo}")
 
     else:
         st.radio(
-            label='Escolha o modo:',
-            options=['Duelo simples', 'Torneio'],
+            label="Escolha o modo:",
+            options=["Duelo simples", "Torneio"],
             horizontal=True,
             disabled=True,
         )
         st.write(
-            'Você precisa ter mais de um jogador e pelo menos dois'
-            'decks adicionados para escolher o modo de jogo.'
+            "Você precisa ter mais de um jogador e pelo menos dois"
+            "decks adicionados para escolher o modo de jogo."
         )
 
     # Seleção se os decks podem ser iguais
     if len(st.session_state.players) > 1 and len(st.session_state.decks) >= 2:
         repeat_deck = st.radio(
-            label='Os Decks entre os adversarios podem ser o mesmo?:',
-            options=['sim', 'nao'],
+            label="Os Decks entre os adversarios podem ser o mesmo?:",
+            options=["sim", "nao"],
             horizontal=True,
-            index=1
+            index=1,
         )
     else:
         repeat_deck = st.radio(
-            label='Os Decks entre os adversarios podem ser o mesmo?:',
-            options=['sim', 'nao'],
+            label="Os Decks entre os adversarios podem ser o mesmo?:",
+            options=["sim", "nao"],
             horizontal=True,
             disabled=True,
         )
         st.write(
-            'Você precisa ter mais de um jogador e pelo menos dois decks'
-            'adicionados para optar se os decks poderão se repetir.'
+            "Você precisa ter mais de um jogador e pelo menos dois decks"
+            "adicionados para optar se os decks poderão se repetir."
         )
 
-    # if len(st.session_state.players) > 1 and len(st.session_state.decks) >= 2:
-    #     if
-    #     if modo == 'Duelo simples':
-    #         duel = DuelsType().singleDuel(
-    #             players=player_manager,
-    #             decks=deck_manager
-
-    #         )
+    if len(st.session_state.players) > 1 and len(st.session_state.decks) >= 2:
+        if modo:
+            if st.button("Processar"):
+                if repeat_deck == "sim":
+                    repeat = True
+                else:
+                    repeat = False
+                if modo == "Duelo simples":
+                    duel = DuelsType().singleDuel(
+                        players=st.session_state["players"],
+                        decks=st.session_state["decks"],
+                        players_last_duel=last_duel,
+                        used_decks={},
+                        decks_can_repeat=repeat,
+                    )
+                    st.write(duel)
