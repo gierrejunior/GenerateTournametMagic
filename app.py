4875f674bb0f661bffc307daffbf457f81ab745c
Gierre Jr <gierremartins@gmail.com>
2024-02-17 20:41:05 -0300

    fix bugs

--- app.py
+++ app.py
@@ -1,234 +1,214 @@
+import base64
+import os
+import random
+
 import streamlit as st
+from streamlit_extras.app_logo import add_logo
+from streamlit_extras.stoggle import stoggle
 
 from interfaces.data_manager import DataManager
 from magic_modules.generator_magic_duels import DuelsType
 
-# from magic_modules.generator_magic_duels import DuelsType
+
+def autoplay_loop_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode()
+    md = f"""
+    <audio controls autoplay loop>
+    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+    </audio>
+    """
+    st.markdown(md, unsafe_allow_html=True)
+
+
+def listar_arquivos(diretorio):
+    lista_de_arquivos = []
+    for filename in os.listdir(diretorio):
+        lista_de_arquivos.append(f'{diretorio}/{filename}')
+    return lista_de_arquivos
+
+
+image_screen_top = 'images/general/01.jpeg'
+autoplay_loop_audio("sound/YuGiOh-Full.mp3")
+st.image(image_screen_top, use_column_width=True)
+add_logo('images/general/02.jpg')
+
 
 if __name__ == "__main__":
-    modo = ""
+    yes_or_no = ["Sim", "Não"]
+    is_random_decks = False
+    imgs_yugioh = listar_arquivos('images/yu_gi_oh')
+    imgs_general = listar_arquivos('images/general')
+    imgs_magic = listar_arquivos('images/magic_the_gathering')
+    imgs_embaralhando = listar_arquivos('images/embaralhando')
+    imgs_alone = listar_arquivos('images/alone')
+
     # Adiciona jogadores
     player_name_input = st.text_input(label="Digite o nome do Magiqueiro(s):")
-    if st.button("Adicionar Magiqueiro"):
+
+    col1, col2 = st.columns(2)
+
+    if col1.button("Adicionar Magiqueiro(s)"):
         DataManager().addPlayer(player_name_input)
-        st.write("Lista final de Magiqueiros:", st.session_state.players)
-    if st.button("Remover Último nome"):
-        DataManager().removeLastPlayer()
-        st.write("Lista final de Magiqueiros:", st.session_state.players)
 
     if st.session_state.get("players"):
+        if col2.button("Remover Último nome"):
+            DataManager().removeLastPlayer()
 
-        # pergunta se quer aleatorizar os decks tambem
-        is_random_decks = st.checkbox("Aleatorizar Decks tambem?")
+        players_string = ', '.join(st.session_state.players)
+        stoggle("Lista de Magiqueiros:", players_string)
 
-        # adiciona decks para serem aleatorizados
-        if is_random_decks:
-            if st.checkbox("Registrar deck(s) Communal(is)"):
-                # Adiciona decks Comunais
-                common_deck_input = st.text_input(label="Digite os decks em comum:")
-                if st.button("Adicionar Deck(s) Comunal(is)"):
-                    DataManager().addCommonDeck(common_deck_input)
-                    st.write(st.session_state.players_and_decks)
-
-                if st.button("Remover Último deck em comum"):
-                    DataManager().removeLastCommonDeck()
-                    st.write(st.session_state.players_and_decks)
-
-            # Verifica se há decks individuais a serem adicionados
-            if st.checkbox("Registrar decks Individuais"):
-                # Pergunta ao usuário qual jogador deseja associar os decks individuais
-                selected_player = st.selectbox(
-                    "Escolha o Magiqueiro:", st.session_state.players
-                )
-
-                individual_decks_input = st.text_input(
-                    label="Digite os decks individuais:"
-                )
-
-                if st.button("Adicionar Deck(s) Individual(is)"):
-                    DataManager().addIndividualDecks(
-                        selected_player, individual_decks_input
-                    )
-                    st.write(st.session_state.players_and_decks)
-
-                if st.button("Remover Último deck individual"):
-                    DataManager().removeLastIndividualDeck(selected_player)
-                    st.write(st.session_state.players_and_decks)
-
-        # Decide se os decks dos adversarios podem ser o mesmo
-        if is_random_decks:
-            can_repeat_deck = st.checkbox(
-                "Os Decks entre os adversários podem ser o mesmo?"
+        if len(st.session_state.get("players")) >= 2:  # type: ignore
+            # Da a opção de randomizar os decks, sim ou não
+            randomize_decks = st.radio(
+                label="Sortear os Decks ?",
+                options=yes_or_no,
+                horizontal=True,
+                index=1,
             )
-        # define os modos que podem ser escolhidos a partir dos requisitos
-        if len(st.session_state.players) > 1:
 
-            if len(st.session_state.players) % 2 == 0:
-                modo = st.radio(
-                    label="Escolha o modo:",
-                    options=["Duelo simples", "Torneio"],
+            if randomize_decks == yes_or_no[0]:
+                is_random_decks = True
+
+            # adiciona decks para serem aleatorizados
+            if is_random_decks:
+                if st.checkbox("Registrar deck(s) Communal(is)"):
+                    # Adiciona decks Comunais
+                    common_deck_input = st.text_input(
+                        label="Digite os decks em comum:")
+                    col1, col2 = st.columns(2)
+
+                    if col1.button("Adicionar Deck(s) Comunal(is)"):
+                        DataManager().addCommonDeck(common_deck_input)
+                        st.write(st.session_state.players_and_decks)
+
+                    if col2.button("Remover Último deck em comum"):
+                        DataManager().removeLastCommonDeck()
+                        st.write(st.session_state.players_and_decks)
+
+                # Verifica se há decks individuais a serem adicionados
+                if st.checkbox("Registrar decks Individuais"):
+                    # Pergunta ao usuário qual jogador deseja associar os decks individuais
+                    selected_player = st.selectbox(
+                        "Escolha o Magiqueiro:", st.session_state.players
+                    )
+
+                    individual_decks_input = st.text_input(
+                        label="Digite os decks individuais:"
+                    )
+
+                    col1, col2 = st.columns(2)
+
+                    if col1.button("Adicionar Deck(s) Individual(is)"):
+                        DataManager().addIndividualDecks(
+                            selected_player, individual_decks_input
+                        )
+                        st.write(st.session_state.players_and_decks)
+
+                    if col2.button("Remover Último deck individual"):
+                        DataManager().removeLastIndividualDeck(selected_player)
+                        st.write(st.session_state.players_and_decks)
+
+                repeat_deck = st.radio(
+                    label="Os oponentes podem possuir o mesmo deck ?",
+                    options=yes_or_no,
                     horizontal=True,
-                    index=0,
+                    index=1,
                 )
-                st.write(f"Modo escolhido: {modo}")
-            else:
-                modo = st.radio(
-                    label="Escolha o modo:",
-                    options=["Duelo simples"],
-                    horizontal=True,
-                    index=0,
-                )
-                st.error(
-                    "O modo torneio só fica ativo se o número de jogadores for par."
-                )
-                st.write(f"Modo escolhido: {modo}")
 
-        else:
-            st.write(
-                "Você precisa ter mais de um jogador para escolher o modo de jogo."
-            )
+                if repeat_deck == yes_or_no[1]:
+                    can_repeat_deck = True
 
-        if (modo == "Duelo simples") and (not is_random_decks):
-            # Pergunta se quer adicioanr os jogadores do ultimo duelo
-            is_add_last_duel = st.checkbox(
-                "Gostaria de adicionar os jogadores do ultimo duelo?"
-            )
-
-            # Adiciona uma janela para selecionar os dois players do último duelo
-            players_last_duel = []
-            if is_add_last_duel:
-                players_last_duel = st.multiselect(
-                    label="Último duelo", options=st.session_state.players
-                )
-                if len(players_last_duel) > 2:
-                    st.error(
-                        "Por favor, selecione no máximo dois players"
-                        " que duelaram no último jogo"
-                    )
-                elif len(players_last_duel) == 2:
-                    st.write(f"Último duelo: {', '.join(players_last_duel)}")
-
-        if (modo == "Duelo simples") and (is_random_decks):
-
-            # Pergunta se quer adicioanr os jogadores do ultimo duelo
-            is_add_last_duel = st.checkbox(
-                "Gostaria de adicionar os jogadores do ultimo duelo?"
-            )
-            # Adiciona uma janela para selecionar os dois players do último duelo
-            players_last_duel = []
-            if is_add_last_duel:
-                players_last_duel = st.multiselect(
-                    label="Último duelo", options=st.session_state.players
-                )
-                if len(players_last_duel) > 2:
-                    st.error(
-                        "Por favor, selecione no máximo dois players"
-                        " que duelaram no último jogo"
-                    )
-                elif len(players_last_duel) == 2:
-                    st.write(f"Último duelo: {', '.join(players_last_duel)}")
-
-    # Botão Processar
-    if st.button("Processar"):
-        if modo == "Duelo simples":
+            st.session_state['last_duel'] = []
+            players_last_duel = st.session_state['last_duel']
             if not is_random_decks:
-                st.write(
-                    DuelsType().singleDuel(
+                # Pergunta se quer adicioanr os jogadores do ultimo duelo
+                is_add_last_duel = st.checkbox(
+                    "Gostaria de adicionar os jogadores do ultimo duelo?"
+                )
+
+                # Adiciona uma janela para selecionar os dois players do último duelo
+                if is_add_last_duel:
+                    players_last_duel = st.multiselect(
+                        label="Último duelo", options=st.session_state.players
+                    )
+                    if len(players_last_duel) > 2:
+                        st.error(
+                            "Por favor, selecione no máximo dois players"
+                            " que duelaram no último jogo"
+                        )
+                    elif len(players_last_duel) == 2:
+                        st.write(
+                            f"Último duelo: {', '.join(players_last_duel)}")
+
+            if is_random_decks:
+
+                # Pergunta se quer adicioanr os jogadores do ultimo duelo
+                is_add_last_duel = st.checkbox(
+                    "Gostaria de adicionar os jogadores do ultimo duelo?"
+                )
+                # Adiciona uma janela para selecionar os dois players do último duelo
+                players_last_duel = []
+                if is_add_last_duel:
+                    players_last_duel = st.multiselect(
+                        label="Último duelo", options=st.session_state.players
+                    )
+                    if len(players_last_duel) > 2:
+                        st.error(
+                            "Por favor, selecione no máximo dois players"
+                            " que duelaram no último jogo"
+                        )
+                    elif len(players_last_duel) == 2:
+                        st.write(
+                            f"Último duelo: {', '.join(players_last_duel)}")
+
+            if st.button("Gerar Duelo"):
+                if not is_random_decks:
+                    duel = DuelsType().singleDuel(
                         st.session_state.players,
                         players_last_duel,
                     )
-                )
-            else:
-                st.write(
-                    DuelsType().singleDuel(
+
+                else:
+                    duel = DuelsType().singleDuel(
                         st.session_state.players_and_decks,
                         players_last_duel,
                         can_repeat_deck,
                     )
-                )
 
-    # # Botão processar
-    # if st.button("Processar"):
-    #     if modo == "Duelo simples":
-    #         simple_duel = DuelsType().singleDuel(
-    #             players=st.session_state["players"],
-    #             decks=st.session_state["decks"],
-    #             players_last_duel=last_duel,
-    #             used_decks={},
-    #             decks_can_repeat=repeat,
-    #         )
+                st.write(duel)
 
-    #         duel = simple_duel[0]
-    #         # Escreve a saída formatada
-    #         st.markdown(
-    #             f"<center><h2>Magiqueiro: {duel.get('player1')}</h2><h2> com o Deck: {duel.get('deck1')}</h2><h1>VS</h1><h2> Magiqueiro: {duel.get('player2')}</h2><h2> com o Deck: {duel.get('deck2')}</h2></center>",
-    #             unsafe_allow_html=True,
-    #         )
-    #         st.session_state["Duelo simples"] = duel
+                if duel:
+                    st.write(
+                        'Salvar esse Duelo, como Ultimo duelo realizado?')
+                    col1, col2, col3, col4, col5 = st.columns(5)
+                    if col3.button("Salvar Duelo"):
+                        if not is_random_decks:
+                            st.write('dsfsdfds2')
+                            players_last_duel = duel
+                            if players_last_duel:
+                                st.success(
+                                    f'Ultimo duelo enter {players_last_duel[0]} e'
+                                    f' {players_last_duel[1]} foi salvo')
 
-    #     elif modo == "Torneio":
-    #         if "winners" not in st.session_state:
-    #             st.session_state["winners"] = st.session_state["players"]
+                        else:
+                            st.write('dsfsdfds3')
+                            players_last_duel = [
+                                duel.get('player_1'), duel.get('player_2')]
 
-    #         if "used_decks" not in st.session_state:
-    #             st.session_state["used_decks"] = {
-    #                 player: [] for player in st.session_state["players"]
-    #             }
+                            if players_last_duel:
+                                st.success(
+                                    f'Ultimo duelo enter {players_last_duel[0]} e'
+                                    f' {players_last_duel[1]} foi salvo')
 
-    #         duels = DuelsType().tournamentDuel(
-    #             players=st.session_state["winners"],
-    #             decks=st.session_state["decks"],
-    #             used_decks=st.session_state["used_decks"],
-    #             decks_can_repeat=repeat,
-    #         )
+            imgs_magic_yugioh = imgs_magic + imgs_yugioh + imgs_embaralhando
+            choice_img = random.choice(imgs_magic_yugioh)
+            st.image(choice_img, use_column_width=True)
 
-    #         st.session_state["torneio"] = duels
-
-    # if "torneio" in st.session_state:
-    #     duels = st.session_state["torneio"]
-    #     # Imprime os duelos e pede ao usuário para escolher o vencedor de cada duelo
-    #     winners = []
-    #     for i, duel in enumerate(duels):
-    #         st.markdown(
-    #             f"## Duelo {i+1}\n\n**{duel['player1']}** usando **{duel['deck1']}** VS **{duel['player2']}** usando **{duel['deck2']}**"
-    #         )
-    #         winner = st.selectbox(
-    #             f"Escolha o vencedor do duelo {i+1}",
-    #             [duel["player1"], duel["player2"]],
-    #             key=f"duel_{i}",
-    #         )
-
-    #         # Salva a seleção do vencedor no session_state
-    #         st.session_state[f"winner_{i}"] = winner
-
-    #     if st.button("Finalizar"):
-    #         winners = [st.session_state[f"winner_{i}"] for i in range(len(duels))]
-
-    #         # Atualiza o dicionário used_decks após cada duelo
-    #         for i in range(len(duels)):
-    #             winner = st.session_state[f"winner_{i}"]
-    #             deck = (
-    #                 duels[i]["deck1"]
-    #                 if duels[i]["player1"] == winner
-    #                 else duels[i]["deck2"]
-    #             )
-    #             st.session_state["used_decks"][winner].append(deck)
-
-    #         # Realiza um novo torneio com os vencedores
-    #         st.session_state["winners"] = winners
-
-    #         if repeat:
-    #             st.session_state["torneio"] = DuelsType().randomizeDecksCanRepeat(
-    #                 decks=st.session_state["decks"],
-    #                 players=st.session_state["winners"],
-    #                 used_decks=st.session_state["used_decks"],
-    #             )
-    #         else:
-    #             st.session_state["torneio"] = DuelsType().randomizeDecksNoRepeat(
-    #                 decks=st.session_state["decks"],
-    #                 players=st.session_state["winners"],
-    #                 used_decks=st.session_state["used_decks"],
-    #             )
-
-    #         # Atualiza a variável duels para a próxima rodada de duelos
-    #         duels = st.session_state["torneio"]
+        else:
+            st.warning(
+                "Você precisa ter mais de um jogador para gerar um duelo."
+            )
+            choice_img = random.choice(imgs_alone)
+            st.image(choice_img, use_column_width=True)
--- magic_modules/generator_magic_duels.py
+++ magic_modules/generator_magic_duels.py
@@ -1,159 +1,17 @@
-import copy
 import random
-from typing import Dict, List
+from typing import Dict, List, Union
 
 
-class RandomizeList:
-    """_summary_"""
-
-    def randomizeList(self, times: int, _list: list) -> list:
-        """_summary_
-
-        Args:
-            times (_type_): _description_
-            _list (_type_): _description_
-
-        Raises:
-            ValueError: _description_
-
-        Returns:
-            _type_: _description_
-        """
-        if not _list:
-            raise ValueError(
-                "RandomizeList: The list is empty."
-                " You cannot randomize an empty list."
-            )
-        result_list = []
-        for i in range(times):
-            result = random.choice(_list)
-            _list.remove(result)
-            result_list.append(result)
-        return result_list
-
-    def randomizelists(self, *args: list) -> list:
-        """_summary_
-
-        Raises:
-            ValueError: _description_
-
-        Returns:
-            _type_: _description_
-        """
-        result_list = []
-        deck_choiced = ""
-        for _list in args:
-            if not _list:
-                raise ValueError(
-                    "RandomizeList: The list is empty."
-                    " You cannot randomize an empty list."
-                )
-            if deck_choiced:
-                if deck_choiced in _list:
-                    _list.remove(deck_choiced)
-            result = random.choice(_list)
-            deck_choiced = result
-            result_list.append(result)
-        return result_list
-
-
-class GeneratorMagicDuels(RandomizeList):
-
-    def randomizePlayers(self, players: list, players_last_duel: list = []) -> List:
-        temp_players = copy.deepcopy(players)
-        temp_player_last_duel = copy.deepcopy(players_last_duel)
-        if not temp_players:
-            raise ValueError(
-                "randomizePlayers: The list is empty."
-                "You cannot randomize an empty list."
-            )
-
-        if temp_player_last_duel:
-            excluded_player = random.choice(temp_player_last_duel)
-            temp_players.remove(excluded_player)
-
-        return self.randomizeList(2, temp_players)
-
-    def randomizeDecksNoRepeat(
-        self, decks: list, players: list, used_decks: dict = {}
-    ) -> List:
-
-        if not decks:
-            raise ValueError(
-                "randomizeDecks: The list is empty."
-                " You cannot randomize an empty list."
-            )
-
-        decks_to_choose = []
-        for player in players:
-            temp_deck = copy.deepcopy(decks)
-            if used_decks:
-                player_used_decks = used_decks.get(player)
-                if player_used_decks:
-                    for player_used_deck in player_used_decks:
-                        temp_deck.remove(player_used_deck)
-            if not temp_deck:
-                raise ValueError(
-                    f"randomizeDecks: The list is empty."
-                    f" the player: {player} has already played"
-                    f" with all available decks."
-                )
-            decks_to_choose.append(temp_deck)
-        final_result_decks = self.randomizelists(*decks_to_choose)
-
-        return final_result_decks
-
-    def randomizeDecksCanRepeat(
-        self, decks: list, players: list, used_decks: dict = {}
-    ) -> List:
-
-        if not decks:
-            raise ValueError(
-                "randomizeDecks: The list is empty."
-                "You cannot randomize an empty list."
-            )
-
-        final_result_decks = []
-        choice_deck = []
-        for player in players:
-            temp_deck = copy.deepcopy(decks)
-            if used_decks:
-                player_used_decks = used_decks.get(player)
-                if player_used_decks:
-                    for player_used_deck in player_used_decks:
-                        temp_deck.remove(player_used_deck)
-            choice_deck = self.randomizeList(1, temp_deck)
-            final_result_decks.append(choice_deck)
-
-        return final_result_decks
-
-    def generateDictDuel(self, players: List, decks: List) -> List[Dict]:
-        if not players or not decks:
-            raise ValueError(
-                "generateDictDuel: The list is empty."
-                " You cannot randomize an empty list."
-            )
-
-        duel = [
-            {
-                "player1": players[i],
-                "deck1": decks[i],
-                "player2": players[i + 1],
-                "deck2": decks[i + 1],
-            }
-            for i in range(0, len(players), 2)
-        ]
-        return duel
-
-
-class DuelsType(GeneratorMagicDuels):
+class DuelsType():
 
     def singleDuel(
         self,
-        players_or_players_and_decks: dict[str, list[str]] | list[str],
-        players_last_duel: list = [],
+        players_or_players_and_decks: Union[Dict[str, List[str]], List[str]],
+        players_last_duel: List = [],
         decks_can_repeat: bool = False,
-    ) -> dict[str, str] | list[str]:
+        used_decks: Dict[str, List[str]] = {}
+    ) -> Union[Dict[str, str], List[str]]:
+
         if isinstance(players_or_players_and_decks, list):
             players = players_or_players_and_decks
             drawn_players = random.sample(players, 2)
@@ -168,19 +26,22 @@ class DuelsType(GeneratorMagicDuels):
                 if value not in players_last_duel
             }
 
-            drawn_players = random.sample(pd_last_duel_removed.keys(), 2)
-            drawn_players_and_decks: dict[str, str] = {}
+            drawn_players = random.sample(list(pd_last_duel_removed.keys()), 2)
+            drawn_players_and_decks: Dict[str, str] = {}
             for i, drawn_player in enumerate(drawn_players, start=1):
-                if decks_can_repeat:
-                    drawn_deck = random.choice(players_and_decks[drawn_player])
-                else:
+                available_decks = [
+                    deck
+                    for deck in players_and_decks[drawn_player]
+                    if deck not in used_decks.get(drawn_player, [])
+                ]
+                if not decks_can_repeat:
                     # Remove o deck já sorteado para evitar repetições
                     available_decks = [
                         deck
-                        for deck in players_and_decks[drawn_player]
+                        for deck in available_decks
                         if deck not in drawn_players_and_decks.values()
                     ]
-                    drawn_deck = random.choice(available_decks)
+                drawn_deck = random.choice(available_decks)
 
                 key_player = f"player_{i}"
                 key_deck = f"deck_{i}"
@@ -188,53 +49,3 @@ class DuelsType(GeneratorMagicDuels):
                 drawn_players_and_decks[key_deck] = drawn_deck
 
             return drawn_players_and_decks
-
-
-    def tournamentDuel(
-        self,
-        players: list,
-        common_decks: list,
-        decks_can_repeat: bool = False,
-    ):
-        pass
-        # temp_players = copy.deepcopy(players)
-        # tournament = []
-        # n_players = len(players)
-        # n_brackets = n_players // 2
-
-        # for player in players:
-        #     player_deck = {player: decks}
-
-        # for bracket in range(n_brackets):
-
-        #     f"bracket{n_brackets}": {}
-
-        #     random_players = self.randomizePlayers(temp_players)
-        #     if isinstance(random_players, ValueError):
-        #         return random_players
-
-        #     new_players_list = [
-        #         player for player in temp_players if player not in random_players
-        #     ]
-
-        #     temp_players = new_players_list
-
-        #     if decks_can_repeat:
-        #         random_decks = self.randomizeDecksCanRepeat(
-        #             decks, random_players, used_decks
-        #         )
-        #     else:
-        #         random_decks = self.randomizeDecksNoRepeat(
-        #             decks, random_players, used_decks
-        #         )
-        #     if isinstance(random_decks, ValueError):
-        #         return random_decks
-
-        #     match = self.generateDictDuel(random_players, random_decks)
-        #     if isinstance(match, ValueError):
-        #         return match
-
-        #     # Adicione cada dicionário individualmente à lista do torneio
-        #     tournament.extend(match)
-
-        # return tournament
